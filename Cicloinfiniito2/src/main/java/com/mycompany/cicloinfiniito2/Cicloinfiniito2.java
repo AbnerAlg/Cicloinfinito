104f2071be41e6c8c574fc9a0ac0b97c874c2a3d
Abner Alain  Gil Garcia <124418451+AbnerAlg@users.noreply.github.com>
2023-05-03 16:41:18 -0600

    Se sube archivo

---
+++ Cicloinfiniito2/src/main/java/com/mycompany/cicloinfiniito2/Cicloinfiniito2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cicloinfiniito2;
+
+/**
+ *
+ * @author abner
+ */
+public class Cicloinfiniito2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
