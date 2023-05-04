3d6f09fcad953854b2e466378ab12908667ed601
Abner Alain  Gil Garcia <124418451+AbnerAlg@users.noreply.github.com>
2023-05-04 10:55:16 -0600

    se sube archivo

--- Cicloinfiniito2/src/main/java/com/mycompany/cicloinfiniito2/Cicloinfiniito2.java
+++ Cicloinfiniito2/src/main/java/com/mycompany/cicloinfiniito2/Cicloinfiniito2.java
@@ -8,9 +8,30 @@ package com.mycompany.cicloinfiniito2;
  *
  * @author abner
  */
-public class Cicloinfiniito2 {
-
+public class CicloInfinito {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        while (true) {
+            // Este es un ciclo infinito
+            // Aquí se pueden agregar instrucciones para que se ejecuten indefinidamente
+            metodoVacio1();
+            metodoVacio2();
+            metodoVacio3();
+        }
+    }
+
+    public static void metodoVacio1() {
+        // Este es un método vacío que no hace nada
+        // Aquí se pueden agregar instrucciones si se desea
+    }
+
+    public static void metodoVacio2() {
+        // Este es otro método vacío que no hace nada
+        // Aquí se pueden agregar instrucciones si se desea
+    }
+
+    public static void metodoVacio3() {
+        // Este es un tercer método vacío que no hace nada
+        // Aquí se pueden agregar instrucciones si se desea
     }
 }
+
